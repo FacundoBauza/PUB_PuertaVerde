4f6d5e8e8bcd1155dbe8ecd4980c566e54bc464c
FacundoBauza <31938804+FacundoBauza@users.noreply.github.com>
2023-10-11 21:56:58 -0300

    Arreglo en PedidosProductos
    Se eliminom la clave com puesta y se genero una clave unica

--- DataAccesLayer/Models/Pedidos_Productos.cs
+++ DataAccesLayer/Models/Pedidos_Productos.cs
@@ -3,6 +3,7 @@ using Domain.Entidades;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,12 @@ using System.Threading.Tasks;
 
 namespace DataAccesLayer.Models
 {
-#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
     [Table(name: "Pedido_Producto")]
-    [PrimaryKey(nameof(id_Pedido), nameof(id_Producto))]
     public class Pedidos_Productos
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int idPedidoProducto { get; set; }
         public int id_Pedido { get; set; }
         public int id_Producto { get; set; }
         public string observaciones { get; set; }
