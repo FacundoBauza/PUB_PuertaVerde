a642101198489a902084fbffda6dcfa7471a844f
Franco Cuesta <81826356+FrancoCuesta@users.noreply.github.com>
2023-10-19 01:22:19 -0300

    estadisticas de un producto

---
+++ DataAccesLayer/Implementations/DAL_Estadisticas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using DataAccesLayer.Interface;
+using DataAccesLayer.Models;
+using Domain.DT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
+namespace DataAccesLayer.Implementations
+{
+    public class DAL_Estadisticas : IDAL_Estadisticas
+    {
+        private readonly DataContext _db;
+        public DAL_Estadisticas(DataContext db)
+        {
+            _db = db;
+        }
+
+        public DTProductoEstadistica producto(DTProductoEstadistica value)
+        {
+#pragma warning disable CS8602 // Desreferencia de una referencia posiblemente NULL.
+            // Consulta con una operación "join"
+            var query = from Pedidos_Productos in _db.Pedidos_Productos
+                        join Pedidos in _db.Pedidos
+                        on Pedidos_Productos.id_Pedido equals Pedidos.id_Pedido
+                        where Pedidos_Productos.id_Producto == value.Producto.id_Producto// Agregar la condición WHERE
+                                && Pedidos.fecha_ingreso >= value.Inicio
+                                && Pedidos.fecha_ingreso <= value.Fin
+                        select new
+                        {
+                            id_Producto = Pedidos_Productos.id_Producto
+                            // Agrega más campos según tus necesidades
+                        };
+#pragma warning restore CS8602 // Desreferencia de una referencia posiblemente NULL.
+            // Ejecuta la consulta y obtén los resultados
+            var results = query.ToList();
+            // Ahora puedes trabajar con los resultados
+            foreach (var result in results)
+            {
+                value.Cantidad++;
+            }
+            return value;
+        }
+    }
+}
